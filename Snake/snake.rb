45fdb5b86175d9bcd3135995cc84571eddaf856e
Javier Perini <jperini@wormholeit.com>
2019-04-10 12:46:55 -0300

    Agregamos mas elementos al cuerpo de la vibora cuando come, y tambien le agregamos velocidad al comer

--- Snake/snake.rb
+++ Snake/snake.rb
@@ -8,33 +8,83 @@ end
 class Snake
 
   def initialize(init_x, init_y)
-    @image = Gosu::Image.new("media/red_snake.jpg")
+    @body = []
+    (0..10).each do|x|
+      @body.push(body_part(init_x + x, init_y))
+    end
+    
     @x = init_x
     @y = init_y
-    @vel_x = @vel_y = @angle = 0.0
+    @vel = 1.5
+    @vel_x = @vel_y = 0.0
     @large = 1
     @direction = Direction::RIGHT
   end
 
   def draw
-    @image.draw_rot(@x, @y, ZOrder::BACKGROUND, @angle)
+    init_previous_x = @x
+    init_previous_y = @y
+    @body.each do |current_body|
+      # Me guardo la posicion anterior
+      previous_x = current_body.x
+      previous_y = current_body.y
+
+      current_body.set_position(init_previous_x, init_previous_y)
+      current_body.draw
+
+      # Sete la posiciones antet
+      init_previous_y = previous_y
+      init_previous_x = previous_x
+    end
+  end
+
+  def body_part(init_x, init_y)
+    BodyPart.new(init_x, init_y)
   end
 
   def eat(coins)
+    initSize = coins.size
     coins.reject! {|coin| Gosu.distance(@x, @y, coin.x, coin.y) < 7 }
+    can_grow_up = coins.size < initSize;
+    grow_up if can_grow_up
   end
 
   def move(game)
     dic = {
-        LEFT:  -> { @x < 0 ? @x = game.width : @x-= 1},
-        RIGHT: -> { @x > game.width ? @x = 0 : @x+= 1},
-        UP: -> { @y < 0 ? @y = game.height : @y-= 1},
-        DOWN: -> { @y > game.height ? @y = 0 : @y+= 1}
+        LEFT:  -> { @x < 0 ? @x = game.width : @x-= @vel},
+        RIGHT: -> { @x > game.width ? @x = 0 : @x+= @vel},
+        UP: -> { @y < 0 ? @y = game.height : @y-= @vel},
+        DOWN: -> { @y > game.height ? @y = 0 : @y+= @vel}
     }
     dic[@direction.to_sym].call
   end
 
   def change_direction(next_direction)
     @direction = next_direction.change_direction(@direction)
+  end
+
+  def grow_up
+    @vel += 0.5
+    @body.push(body_part(@x, @y))
+  end
+end
+
+class BodyPart
+  attr_reader :x, :y
+
+  def initialize(init_x, init_y)
+    @image = Gosu::Image.new("media/red_snake.jpg")
+    @x = init_x
+    @y = init_y
+    @angle = 0.0
+  end
+
+  def draw
+    @image.draw_rot(@x, @y, ZOrder::BACKGROUND, @angle)
+  end
+
+  def set_position(x, y)
+    @x = x
+    @y = y
   end
 end
